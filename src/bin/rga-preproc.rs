f53688269ea041be5bfee218f97d2d4d47bcd951
phiresky <phireskyde+git@gmail.com>
2019-06-05 11:39:35 +0200

    simplify somewhat

--- src/bin/rga-preproc.rs
+++ src/bin/rga-preproc.rs
@@ -5,63 +5,46 @@ use std::fmt;
 use std::path::Path;
 use tree_magic;
 
-#[derive(Debug)]
-struct ShittyError;
-
-impl Error for ShittyError {}
-
-impl fmt::Display for ShittyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "ShittyError")
-    }
+// lazy error
+fn lerr(inp: impl AsRef<str>) -> Box<dyn Error> {
+    return inp.as_ref().into();
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let adapters = init_adapters()?;
-    //let ad = &adapters[0];
-    //let z: &str = &ad.metadata().name;
+    let filepath = std::env::args()
+        .skip(1)
+        .next()
+        .ok_or(lerr("No filename specified"))?;
+    println!("fname: {}", filepath);
+    let path = Path::new(&filepath);
+    let filename = path.file_name().ok_or(lerr("Empty filename"))?;
 
-    // todo: how to make this less indenty?
-    match std::env::args().skip(1).next() {
-        Some(filepath) => {
-            println!("fname: {}", filepath);
-            let path = Path::new(&filepath);
-            let maybe_filename = path.file_name();
-            match maybe_filename {
-                Some(filename) => {
-                    let result = tree_magic::from_filepath(path);
-                    match result {
-                        Some(mimetype) => {
-                            println!("mimetype: {:?}", mimetype);
-                            let adapter = adapters(FileMeta {
-                                mimetype,
-                                lossy_filename: filename.to_string_lossy().to_string(),
-                            });
-                            match adapter {
-                                Some(ad) => {
-                                    println!("adapter: {}", &ad.metadata().name);
-                                    let stdouti = std::io::stdout();
-                                    let mut stdout = stdouti.lock();
-                                    ad.adapt(&filepath, &mut stdout)?;
-                                    Ok(())
-                                }
-                                None => {
-                                    eprintln!("no adapter for that file, running cat!");
-                                    let stdini = std::io::stdin();
-                                    let mut stdin = stdini.lock();
-                                    let stdouti = std::io::stdout();
-                                    let mut stdout = stdouti.lock();
-                                    std::io::copy(&mut stdin, &mut stdout)?;
-                                    Ok(())
-                                }
-                            }
-                        }
-                        None => Err("file does not exist".into()),
-                    }
-                }
-                None => Err("Empty filename".into()),
-            }
+    let mimetype = tree_magic::from_filepath(path).ok_or(lerr(format!(
+        "File {} does not exist",
+        filename.to_string_lossy()
+    )))?;
+    println!("mimetype: {:?}", mimetype);
+    let adapter = adapters(FileMeta {
+        mimetype,
+        lossy_filename: filename.to_string_lossy().to_string(),
+    });
+    match adapter {
+        Some(ad) => {
+            println!("adapter: {}", &ad.metadata().name);
+            let stdouti = std::io::stdout();
+            let mut stdout = stdouti.lock();
+            ad.adapt(&filepath, &mut stdout)?;
+            Ok(())
         }
-        None => Err("No filename specified".into()),
+        None => {
+            eprintln!("no adapter for that file, running cat!");
+            let stdini = std::io::stdin();
+            let mut stdin = stdini.lock();
+            let stdouti = std::io::stdout();
+            let mut stdout = stdouti.lock();
+            std::io::copy(&mut stdin, &mut stdout)?;
+            Ok(())
+        }
     }
 }
