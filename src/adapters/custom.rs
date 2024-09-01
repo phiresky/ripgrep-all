d8aaa0ea5ad386a484c9017011e229ea1364c93b
Joseph LaFreniere <git@lafreniere.xyz>
2024-09-01 10:46:11 -0400

    Add missing newline in docstring

--- src/adapters/custom.rs
+++ src/adapters/custom.rs
@@ -45,6 +45,7 @@ pub struct CustomAdapterConfig {
     /// - $input_file_extension: the file extension (without dot). e.g. foo.tar.gz -> gz
     /// - $input_file_stem, the file name without the last extension. e.g. foo.tar.gz -> foo.tar
     /// - $input_virtual_path: the full input file path. Note that this path may not actually exist on disk because it is the result of another adapter
+    ///
     /// stdin of the program will be connected to the input file, and stdout is assumed to be the converted file
     pub args: Vec<String>,
     /// The output path hint. The placeholders are the same as for `.args`
